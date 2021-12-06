97653d65df84b0d2615ee270273cde63fbeb0198
rafat69ahmed <rafat69ahmed@gmail.com>
2021-12-06 14:48:46 +0600

    user controller added and env variable used

---
+++ app/controllers/user_controller.js
@@ -0,0 +1,8 @@
+
+const author_list = function(req, res) {
+    res.json('NOT IMPLEMENTED: Author list');
+};
+
+module.exports = {
+    author_list
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../app/controllers/user_controller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express test' });
-});
+router.get('/', userController.author_list);
 
 module.exports = router;
